4bc98151df6cc2554ce9a63591d896dd6b2ba4e6
Binbin Weng <wbb0721@gmail.com>
2024-06-17 18:11:55 -0400

    update budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,11 +3,34 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+    const { expenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    //const [action, setAction] = useState('');
     const handleBudgetChange = (event) => {
+        
         setNewBudget(event.target.value);
+        if(newBudget>20000){
+            alert("The value cannot exceed maximum fund £ 20000");
+            setNewBudget(budget);
+            return;
+        }
+        
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+        if(newBudget<totalExpenses){
+            alert("You cannot reduce the budget value lower than the spending £" + totalExpenses);
+            setNewBudget(budget);
+            return;
+        }
+        
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget,
+        });
     }
+    
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
